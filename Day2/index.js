f2e1968ff40bef08fe3fd7b01e03a63b617f9a36
intellij <patilprajwal8804@gmail.com>
2026-03-16 02:45:47 +0530

    day 2 Complete

---
+++ Day2/index.js
@@ -0,0 +1,20 @@
+
+let heading = document.querySelector("h1");
+let cartItemCount = 0;
+heading.innerText = cartItemCount;
+
+function addToCart()
+{
+    cartItemCount++;
+    heading.innerText = cartItemCount;
+} 
+function originalValue()
+{
+    console.log(cartItemCount);
+}
+
+function reset()
+{
+    cartItemCount = 0;
+    heading.innerText = cartItemCount;
+}
