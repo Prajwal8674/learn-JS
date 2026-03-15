a0af059ff2ddc3f986664ce6283947b804667e66
intellij <patilprajwal8804@gmail.com>
2026-03-16 02:21:25 +0530

    First commit

---
+++ Day1 Variables/index.js
@@ -0,0 +1,17 @@
+var a = 10;
+console.log(a);
+
+var name="Prajwal";
+console.log(name);
+
+var flag= true;
+console.log(flag);
+
+var x=10;
+ x= 20;
+console.log(x);
+
+let l = 10 ;
+console.log(l);
+
+let  
